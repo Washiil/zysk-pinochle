1fb2a08677bcfd3cd279ed9d33af302a73db0b57
Washi <65116802+Washiil@users.noreply.github.com>
2025-12-24 16:42:40 -0500

    implemented masks and shifts for efficient bit math

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -14,3 +14,29 @@ pub enum Suit {
 pub enum Rank {
     Nine, Jack, Queen, King, Ten, Ace,
 }
+
+impl Suit {
+    #[inline(always)]
+    pub const fn shift(self) -> u32 {
+        match self {
+            Suit::Spades => 0,
+            Suit::Hearts => 12,
+            Suit::Diamonds => 24,
+            Suit::Clubs => 36,
+        }
+    }
+}
+
+impl Rank {
+    #[inline(always)]
+    pub const fn mask(self) -> u64 {
+        match self {
+            Rank::Nine => 0b000000000011,
+            Rank::Jack => 0b000000001100,
+            Rank::Queen => 0b000000110000,
+            Rank::King => 0b000011000000,
+            Rank::Ten => 0b001100000000,
+            Rank::Ace => 0b110000000000,
+        }
+    }
+}
